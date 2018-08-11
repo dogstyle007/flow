5ab9bd541fe8ca4b0f8e722c95079753549e5650
dogstyle007 <johnbosco.benedict8@gmail.com>
2018-08-11 13:02:28 +0000

    Updated site files and functions

--- config/mail.php
+++ config/mail.php
@@ -120,12 +120,13 @@ return [
         ],
     ],
 
-    'stream' => [
+    /*'stream' => [
         'ssl' => [
             'allow_self_signed' => true,
             'verify_peer' => false,
             'verify_peer_name' => false,
         ],
     ],
+    */
 
 ];
