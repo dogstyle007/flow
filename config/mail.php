7068856b14cf21e5c10633670a9b1e3dd58d215e
dogstyle007 <johnbosco.benedict8@gmail.com>
2018-08-11 13:29:43 +0000

    Updated site files and functions

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('abelwisdom007@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Password@123'),
 
     /*
     |--------------------------------------------------------------------------
@@ -100,6 +100,8 @@ return [
     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
+
+    'pretend' => false,
 
     /*
     |--------------------------------------------------------------------------
