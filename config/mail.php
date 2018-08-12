a2125faf98779fb639c5cc60c8e11a73d759c111
dogstyle007 <johnbosco.benedict8@gmail.com>
2018-08-12 23:00:22 +0000

    Updated site files and functions

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', '4kidstv007@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'demo@blog.johnboscotips.com '),
         'name' => env('MAIL_FROM_NAME', 'Old Vandals Association'),
     ],
 
